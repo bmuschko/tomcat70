9cf2521d0ffe5a83eb4ad18831b9d8101a5d3aab
bmuschko <benjamin.muschko@gmail.com>
2012-04-28 18:33:13 -0400

    Added missing CGLib library and cleaned up test class.

--- test/org/apache/catalina/loader/TestHandleTypesWebappLoader.java
+++ test/org/apache/catalina/loader/TestHandleTypesWebappLoader.java
@@ -6,9 +6,10 @@ import org.apache.catalina.startup.TomcatBaseTest;
 import org.junit.Test;
 
 import java.io.File;
+import java.net.MalformedURLException;
 
 public class TestHandleTypesWebappLoader extends TomcatBaseTest {
-    public static final String TEST_DIR = "test/webapp-3.0-spring";
+    public static final String TEST_DIR = new File("test/webapp-3.0-spring").getAbsolutePath();
 
     @Test
     public void testStartInternalForLibrary() throws Exception {
@@ -16,15 +17,11 @@ public class TestHandleTypesWebappLoader extends TomcatBaseTest {
         StandardContext context = (StandardContext)tomcat.addWebapp(null, "/spring", TEST_DIR + "/src/main/webapp");
         context.setLoader(new WebappLoader());
         context.setUnpackWAR(false);
+
+        // Add application classes as JAR file
         context.getLoader().addRepository(new File(TEST_DIR, "/target/lib/app.jar").toURI().toURL().toString());
 
-        File folder = new File(TEST_DIR, "/lib");
-        File[] libs = folder.listFiles();
-
-        for(File lib : libs) {
-            context.getLoader().addRepository(lib.toURI().toURL().toString());
-        }
-
+        addLibsToRepository(context);
         tomcat.start();
     }
 
@@ -34,16 +31,21 @@ public class TestHandleTypesWebappLoader extends TomcatBaseTest {
         StandardContext context = (StandardContext)tomcat.addWebapp(null, "/spring", TEST_DIR + "/src/main/webapp");
         context.setLoader(new WebappLoader());
         context.setUnpackWAR(false);
+
+        // Add application classes as directory
         context.getLoader().addRepository(new File(TEST_DIR, "/target/classes/main").toURI().toURL().toString());
         context.getLoader().addRepository(new File(TEST_DIR, "/src/main/resources").toURI().toURL().toString());
 
+        addLibsToRepository(context);
+        tomcat.start();
+    }
+
+    private void addLibsToRepository(StandardContext context) throws MalformedURLException {
         File folder = new File(TEST_DIR, "/lib");
         File[] libs = folder.listFiles();
 
         for(File lib : libs) {
             context.getLoader().addRepository(lib.toURI().toURL().toString());
         }
-
-        tomcat.start();
     }
 }
